06ac0804d48131ec37f2a66401f9b717502d595e
Titrom25 <titrom25@mail.ru>
2022-02-01 18:59:43 +0300

    Add checkers game field analysis

--- BoardGameAssistant.py
+++ BoardGameAssistant.py
@@ -1,11 +1,24 @@
+import cv2
 import json
 from tgbot import tgbot
 from gameClassifier import gameClassifier
+from checkersDetector import checkersDetector
+
+detector = checkersDetector.CheckersDetector(pathToYolo='checkersDetector/yolov5', pathToModel='checkersDetector/detector.pt')
+
+def detect_checkers(image_path, chat_id):
+	img = cv2.imread(image_path)
+	res = detector.getGameField(img,visualize=True)
+	cv2.imwrite("layout.jpg", res[1])
+	cv2.imwrite("game.jpg", res[0])
+	bot.send_message(chat_id=chat_id, text='Detected layout:')
+	bot.send_photo(chat_id=chat_id, photo=open("layout.jpg", 'rb'))
+	bot.send_message(chat_id=chat_id, text='Digitized game:')
+	bot.send_photo(chat_id=chat_id, photo=open("game.jpg", 'rb'))
 
 CONFIG_PATH = "config.json"
 with open(CONFIG_PATH) as f:
 	config = json.load(f)
 
 BOT_TOKEN = config['TG_BOT_KEY']
-
-tgbot.initBot(token=BOT_TOKEN, classifyGameFunction=gameClassifier.classifyGameImage)
+bot = tgbot.initBot(token=BOT_TOKEN, classifyGameFunction=gameClassifier.classifyGameImage, detect_checkersFunction=detect_checkers)
---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM condaforge/miniforge3:4.10.3-10
+
+WORKDIR /app
+RUN apt-get --allow-releaseinfo-change update && apt-get -y install gcc
+
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get install -y python3-opencv
+
+RUN conda create -n Boardgame python=3.8
+RUN /opt/conda/envs/Boardgame/bin/pip install torch==1.8.1 torchvision==0.9.1
+
+COPY env.yml .
+RUN conda env update --name Boardgame --file env.yml
+
+COPY . .
+CMD conda run -n Boardgame /bin/bash -c "python -u BoardGameAssistant.py &"
