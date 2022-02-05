1c10cb581e04af3f0c2ecd8776b480f88a669366
Titrom25 <titrom25@mail.ru>
2022-02-05 20:37:15 +0700

    Add carcassone submodule support

--- BoardGameAssistant.py
+++ BoardGameAssistant.py
@@ -1,11 +1,23 @@
 import cv2
 import json
+from PIL import Image
 from tgbot import tgbot
-from tictactoe import pipeline
 from gameClassifier import gameClassifier
+from tictactoe import pipeline
 from checkers import checkersDetector
+from carcassone import tiles_board
 
 detector = checkersDetector.CheckersDetector(pathToYolo='checkers/yolov5', pathToModel='checkers/models/detector.pt')
+
+def detect_tictactoe(image_path, chat_id):
+	result = pipeline.execute_pipline(image_path=image_path)
+	print(len(result))
+	if result[0] == 'Cannot detect game properly':
+		bot.send_message(chat_id=chat_id, text='Unable to recognize game situation')
+	else:
+		cv2.imwrite("ttt.jpg", result[0])
+		bot.send_photo(chat_id=chat_id, photo=open("ttt.jpg", 'rb'))
+
 
 def detect_checkers(image_path, chat_id, top_white):
 	img = cv2.imread(image_path)
@@ -23,14 +35,33 @@ def detect_checkers(image_path, chat_id, top_white):
 	bot.send_message(chat_id=chat_id, text='Suggested black move:')
 	bot.send_photo(chat_id=chat_id, photo=open("res_black.jpg", 'rb'))
 
-def detect_tictactoe(image_path, chat_id):
-	result = pipeline.execute_pipline(image_path=image_path)
-	print(len(result))
-	if result[0] == 'Cannot detect game properly':
-		bot.send_message(chat_id=chat_id, text='Unable to recognize game situation')
-	else:
-		cv2.imwrite("ttt.jpg", result[0])
-		bot.send_photo(chat_id=chat_id, photo=open("ttt.jpg", 'rb'))
+
+def detect_carcassone(field_image_path, card_image_path, chat_id):
+	field_image = Image.open(field_image_path)
+	card_image = Image.open(card_image_path)
+
+	board = tiles_board.CarcassoneBoard('carcassone/models/detection_model.pt', 'carcassone/models/cls_model.pt')
+	board_img = board.recognize_game_situation(field_image)
+
+	cv2.imwrite("board_img.jpg", board_img)
+	bot.send_message(chat_id=chat_id, text='Detected board:')
+	bot.send_photo(chat_id=chat_id, photo=open("board_img.jpg", 'rb'))
+
+	positions_im, tile_im = board.get_possible_positions(card_image)
+	
+	tile_im.save("tile_im.jpg")
+	bot.send_message(chat_id=chat_id, text='Detected tile:')
+	bot.send_photo(chat_id=chat_id, photo=open("tile_im.jpg", 'rb'))
+
+	cv2.imwrite("positions_im.jpg", positions_im)
+	bot.send_message(chat_id=chat_id, text='Available tile positions:')
+	bot.send_photo(chat_id=chat_id, photo=open("positions_im.jpg", 'rb'))
+
+	tiles_left_im = board.get_tiles_left()
+	tiles_left_im.save("tiles_left_im.jpg")
+	bot.send_message(chat_id=chat_id, text='Tiles available:')
+	bot.send_photo(chat_id=chat_id, photo=open("tiles_left_im.jpg", 'rb'))
+
 
 CONFIG_PATH = "config.json"
 with open(CONFIG_PATH) as f:
@@ -41,6 +72,7 @@ BOT_TOKEN = config['TG_BOT_KEY']
 handlers_fns = {
 	'classifyGame': gameClassifier.classifyGameImage, 
 	'detect_checkers': detect_checkers,
-	'detect_tictactoe': detect_tictactoe
+	'detect_tictactoe': detect_tictactoe,
+	'detect_carcassone': detect_carcassone
 }
 bot = tgbot.initBot(token=BOT_TOKEN, handlers_fns=handlers_fns)
