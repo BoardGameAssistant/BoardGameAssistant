d1abab698e309895a770a94d1279c95d9680086e
Titrom25 <titrom25@mail.ru>
2022-02-15 17:37:45 +0700

    Add image caching

--- BoardGameAssistant.py
+++ BoardGameAssistant.py
@@ -25,21 +25,25 @@ def detect_checkers(image_path, chat_id, top_white):
 	img = cv2.imread(image_path)
 	visual, layout, res_white, res_black  = detector.getGameField(img,visualize=True, roll=top_white)
 
-	cv2.imwrite("layout.jpg", layout)
+	layout_img_path = image_path.replace('image', 'checkers_layout').replace('.png', '.jpg')
+	cv2.imwrite(layout_img_path, layout)
 	bot.send_message(chat_id=chat_id, text='Detected layout:')
-	bot.send_photo(chat_id=chat_id, photo=open("layout.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(layout_img_path, 'rb'))
 
-	cv2.imwrite("game.jpg", visual)
+	game_img_path = image_path.replace('image', 'checkers_game').replace('.png', '.jpg')
+	cv2.imwrite(game_img_path, visual)
 	bot.send_message(chat_id=chat_id, text='Digitized game:')
-	bot.send_photo(chat_id=chat_id, photo=open("game.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(game_img_path, 'rb'))
 
-	cv2.imwrite("res_white.jpg", res_white)
+	res_white_img_path = image_path.replace('image', 'checkers_white').replace('.png', '.jpg')
+	cv2.imwrite(res_white_img_path, res_white)
 	bot.send_message(chat_id=chat_id, text='Suggested white move:')
-	bot.send_photo(chat_id=chat_id, photo=open("res_white.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(res_white_img_path, 'rb'))
 	
-	cv2.imwrite("res_black.jpg", res_black)
+	res_black_img_path = image_path.replace('image', 'checkers_black').replace('.png', '.jpg')
+	cv2.imwrite(res_black_img_path, res_black)
 	bot.send_message(chat_id=chat_id, text='Suggested black move:')
-	bot.send_photo(chat_id=chat_id, photo=open("res_black.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(res_black_img_path, 'rb'))
 
 
 def detect_carcassone(field_image_path, card_image_path, chat_id):
@@ -49,24 +53,28 @@ def detect_carcassone(field_image_path, card_image_path, chat_id):
 	board = tiles_board.CarcassoneBoard('carcassone/models/detection_model.pt', 'carcassone/models/cls_model.pt')
 	board_img = board.recognize_game_situation(field_image)
 
-	cv2.imwrite("board_img.jpg", board_img)
+	board_img_path = field_image_path.replace('image', 'board_img').replace('.png', '.jpg')
+	cv2.imwrite(board_img_path, board_img)
 	bot.send_message(chat_id=chat_id, text='Detected board:')
-	bot.send_photo(chat_id=chat_id, photo=open("board_img.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(board_img_path, 'rb'))
 
 	positions_im, tile_im = board.get_possible_positions(card_image)
 	
-	tile_im.save("tile_im.jpg")
+	board_tile_path = field_image_path.replace('image', 'tile_im').replace('.png', '.jpg')
+	tile_im.save(board_tile_path)
 	bot.send_message(chat_id=chat_id, text='Detected tile:')
-	bot.send_photo(chat_id=chat_id, photo=open("tile_im.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(board_tile_path, 'rb'))
 
-	cv2.imwrite("positions_im.jpg", positions_im)
+	board_possible_path = field_image_path.replace('image', 'positions_im').replace('.png', '.jpg')
+	cv2.imwrite(board_possible_path, positions_im)
 	bot.send_message(chat_id=chat_id, text='Available tile positions:')
-	bot.send_photo(chat_id=chat_id, photo=open("positions_im.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(board_possible_path, 'rb'))
 
+	board_tiles_lefte_path = field_image_path.replace('image', 'tiles_left_im').replace('.png', '.jpg')
 	tiles_left_im = board.get_tiles_left()
-	tiles_left_im.save("tiles_left_im.jpg")
+	tiles_left_im.save(board_tiles_lefte_path)
 	bot.send_message(chat_id=chat_id, text='Tiles available:')
-	bot.send_photo(chat_id=chat_id, photo=open("tiles_left_im.jpg", 'rb'))
+	bot.send_photo(chat_id=chat_id, photo=open(board_tiles_lefte_path, 'rb'))
 
 
 if __name__ == "__main__":
